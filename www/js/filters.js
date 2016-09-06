03adbf62838c8d505c09f303619e7e448e016b5f
amespineira <amespineira@me.com>
2016-09-06 14:43:59 -0600

    removed duplicate code

--- www/js/filters.js
+++ www/js/filters.js
@@ -20,19 +20,4 @@ angular.module('starter.filters',[])
     });
     return result;
   }
-})
-.filter('people', function() {
-  return function(input, search) {
-    if (!input) return input;
-    if (!search) return input;
-    var expected = ('' + search).toLowerCase();
-    var result = {};
-    angular.forEach(input, function(value, key) {
-      var actual = ('' + value).toLowerCase();
-      if (actual.indexOf(expected) !== -1) {
-        result[key] = value;
-      }
-    });
-    return result;
-  }
 });
