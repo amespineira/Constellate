94cbd439af681b26d91d27d7eac70f862d00af46
amespineira <amespineira@me.com>
2016-09-09 15:20:46 -0600

    added sort to people display

--- www/js/test.js
+++ www/js/test.js
@@ -1,43 +1,10 @@
-var data = {
-  places: [
-    {
-      id:1,
-      name:'test',
-    }
-  ],
-  people: [
-    {
-      people_id: "2",
-      first_name: "seconda",
-      last_name: "secondb",
-      user_id: '1',
-      place_id: '1',
-      notes: [
-        {
-          "id": "2",
-          "type": "test",
-          "text": "this a note about2"
-        }
-      ],
-      links: [
-        {
-          "link_id": "2",
-          "name": "second",
-          "url": "thing.jpg"
-        }
-      ]
-    }
-  ],
+if ('a'>'b'){
+  console.log('a'>'b');
 }
-
-var places={};
-data.places.forEach(function(place){
-  places[Number(place.id)]={
-    id:Number(place.id),
-    name:place.name,
-    people:[]
-  };
-})
-data.people.forEach(function(person){
-  places[Number(person.place_id)].people.push(person);
-})
+if ('a'<'b'){
+}
+var a="a";
+var b="b"
+console.log((a>b)*1);
+console.log((a<b)*-1);
+console.log(((a>b)*1)+((a<b)*-1));
